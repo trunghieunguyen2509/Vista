26643353a79c32049791e4b2d40c0a35868a19da
Hieu <hieutrungng2509@gmail.com>
2026-07-21 15:45:35 +1000

    commit

--- app.py
+++ app.py
@@ -3,10 +3,45 @@ from flask import Flask, render_template
 # Initialize flask applkication
 app = Flask(__name__)
 
+# 1. Define the currency list data
+currencies = [
+        {"code": "USD", "flag": "us"},
+        {"code": "GBP", "flag": "gb"},
+        {"code": "EUR", "flag": "eu"},
+        {"code": "JPY", "flag": "jp"},
+        {"code": "HKD", "flag": "hk"},
+        {"code": "THB", "flag": "th"},
+        {"code": "SGD", "flag": "sg"},
+        {"code": "NZD", "flag": "nz"},
+        {"code": "FJD", "flag": "fj"},
+        {"code": "CNY", "flag": "cn"},
+        {"code": "CAD", "flag": "ca"},
+        {"code": "IDR", "flag": "id"},
+        {"code": "PHP", "flag": "ph"},
+        {"code": "INR", "flag": "in"},
+        {"code": "AED", "flag": "ae"},
+        {"code": "KRW", "flag": "kr"},
+        {"code": "MYR", "flag": "my"},
+        {"code": "ZAR", "flag": "za"},
+        {"code": "VND", "flag": "vn"},
+        {"code": "TWD", "flag": "tw"},
+        {"code": "CHF", "flag": "ch"},
+        {"code": "TRY", "flag": "tr"},
+        {"code": "XPF", "flag": "pf"},
+        {"code": "DKK", "flag": "dk"},
+        {"code": "SEK", "flag": "se"},
+        {"code": "CZK", "flag": "cz"},
+        {"code": "PLN", "flag": "pl"},
+        {"code": "HUF", "flag": "hu"},
+        {"code": "MXN", "flag": "mx"},
+        {"code": "VUV", "flag": "vu"},
+        {"code": "QAR", "flag": "qa"},
+        {"code": "NOK", "flag": "no"}
+    ]     
 # Define route
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', currencies=currencies)
 
 if __name__ == '__main__':
     app.run(debug=True)
